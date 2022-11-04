df55a800e199e930d0d62af229cdd8ff94f4cac0
Ermanno Fissore <ermannofissore@gmail.com>
2022-11-04 14:14:11 +0000

    Initial Commit

---
+++ PrimaryBid-Connect/PrimaryBid_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimaryBid_ConnectApp.swift
+//  PrimaryBid-Connect
+//
+//  Created by Ermanno Fissore on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PrimaryBid_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
